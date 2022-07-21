959e2c9188f5f3ccce2df56d201ad9c87af969f7
Denilson Behenck <denilsontrespa10@gmail.com>
2022-07-21 13:26:20 -0300

    post

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -6,12 +8,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/behenck.png" />
+
                 <strong>Denilson Behenck</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar Perfil
                 </a>
             </footer>
