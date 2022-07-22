5132b5a489eb2926deca90412d0b5e774e3812d2
Denilson Behenck <denilsontrespa10@gmail.com>
2022-07-22 13:22:26 -0300

    comecando a dar funcionabilidade

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from "./components/Post";
 import "./global.css"
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatar: 'https://github.com/behenck.png',
+      name: 'Denilson Behenck',
+      role: 'Programador Plêno'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-22 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatar: 'https://github.com/diego3g.png',
+      name: 'Mike Brito',
+      role: 'Educador @ROcketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-21 20:00:00'),
+  },
+]
+
 function App() {
   return (
     <div>
@@ -13,7 +44,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          { posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,15 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post(props) {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/behenck.png" />
-          <div  className={styles.authorInfo}>
+          <Avatar src="https://github.com/behenck.png" />
+
+          <div className={styles.authorInfo}>
             <strong>Denilson Behenck</strong>
             <span>Web Developer</span>
           </div>
@@ -37,6 +40,12 @@ export function Post() {
           <button type="submit">Comentar</button> 
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
